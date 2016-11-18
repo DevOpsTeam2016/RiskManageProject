f9bd5d51d584e2a68b51842a21e621228067f346
RAINY <RAINY@gmail.com>
2016-11-18 09:51:45 +0800

    change modify js

--- web/static/js/manager/modify.js
+++ web/static/js/manager/modify.js
@@ -11,12 +11,19 @@ $(".checkOne_index").click(function(){
             a[i]=$(this).text();
         })
         $("#modify").click(function(){
-            console.log("click");
-            console.log(a);
-            $("#modifyModal").find("input").each(function(i){
-                console.log("input");
-                $(this).val(a[i]);
-            })
+            console.log($(".checkOne_index:checked").length);
+            if($(".checkOne_index:checked").length == 1){
+                console.log("click");
+                console.log(a);
+                $("#modifyModal").find("input").each(function(i){
+                    console.log("input");
+                    $(this).val(a[i]);
+                })
+            }else if($(".checkOne_index:checked").length == 0){
+                alert("请选择要修改的条目！！");
+            }else {
+                alert("只能选择一条哦~~");
+            }
         })
     }
 })
